0117f9515ffd5a4ca3180badf881d9431ff09592
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-10-31 16:33:57 +0100

    Ajout de l'arme pour le Player

--- game/game.cs
+++ game/game.cs
@@ -1,10 +1,7 @@
 using Godot;
 
 public partial class game : Node2D {
-    private int _score;
-    // Don't forget to rebuild the project so the editor knows about the new export variable.
-
-    [Export] public PackedScene MobScene { get; set; }
-
     public override void _Ready() {}
+
+    public override void _Process(double delta) {}
 }
