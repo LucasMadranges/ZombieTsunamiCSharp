913044e7360e3e2d4246d3051fe705198b63686d
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-10-31 11:29:32 +0100

    Indentation du code

--- game/game.cs
+++ game/game.cs
@@ -1,12 +1,8 @@
 using Godot;
-using System;
 
-public partial class game : Node2D
-{
+public partial class game : Node2D {
+    private int _score;
     // Don't forget to rebuild the project so the editor knows about the new export variable.
 
-    [Export]
-    public PackedScene MobScene { get; set; }
-
-    private int _score; 
+    [Export] public PackedScene MobScene { get; set; }
 }
