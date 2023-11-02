f2070e50dc7caa143d3301cc9530262824a036cb
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-11-02 16:57:20 +0100

    Ajout des balles lors du clique de la souris pour tirer

--- assets/ennemy/ZombieToast.cs
+++ assets/ennemy/ZombieToast.cs
@@ -13,6 +13,10 @@ public partial class ZombieToast : RigidBody2D {
         var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         var mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
         animatedSprite2D.Play(mobTypes[0]);
+
+        // Choose a random location on Path2D.
+        var mobSpawnLocation = GetTree().CurrentScene.GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+        mobSpawnLocation.ProgressRatio = GD.Randf();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
