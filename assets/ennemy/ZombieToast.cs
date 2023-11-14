30e704e04e88f6426c32081e781955fba69065bb
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-11-14 19:42:11 +0100

    Work in progress zombie attack

--- assets/ennemy/ZombieToast.cs
+++ assets/ennemy/ZombieToast.cs
@@ -1,13 +1,23 @@
 using Godot;
 
-public partial class ZombieToast : RigidBody2D {
+public partial class ZombieToast : CharacterBody2D {
     private const float SPEED = 0.2f;
+
+    [Export] private float aps = 2f;
+
+    private float attack_speed;
+    [Export] private float damage = 10f;
+    private float time_until_attack;
+    private bool within_attack_range;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
         // var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         // var mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
         // animatedSprite2D.Play(mobTypes[0]);
+
+        attack_speed = 1 / aps;
+        time_until_attack = attack_speed;
 
         // Choose a random location on Path2D.
         var mobSpawnLocation = GetTree().CurrentScene.GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
@@ -20,5 +30,25 @@ public partial class ZombieToast : RigidBody2D {
         var targetPosition = (target.Position - Position).Normalized();
         MoveAndCollide(targetPosition * SPEED);
         LookAt(target.GlobalPosition);
+
+        if (within_attack_range)
+            Attack();
+        else
+            time_until_attack -= (float)delta;
+    }
+
+    public void Attack() {
+        GD.Print("Attack player");
+    }
+
+    public void OnAttackBodyEnter(Node2D body) {
+        if (body.IsInGroup("player")) within_attack_range = true;
+    }
+
+    public void OnAttackBodyExit(Node2D body) {
+        if (body.IsInGroup("player")) {
+            within_attack_range = false;
+            time_until_attack = attack_speed;
+        }
     }
 }
