6b9f516dcb2a834266059cd7e5a240dc2ba543c7
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-11-19 14:47:20 +0100

    oublier de push la progress bar rip

---
+++ assets/script/ProgressBar.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using Godot;
+
+public partial class ProgressBar : Godot.ProgressBar {
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) {
+        Value = GetTree().CurrentScene.GetNode<CharacterBody2D>("Player").GetNode<Health>("Health").health;
+    }
+}
